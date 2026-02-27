eac941ba5d6c2c3c7147a82d5f88d900afb0d953
ruben-blok <ruben.blok@icloud.com>
2026-02-27 10:33:34 +0100

    opdracht 1

---
+++ opdracht_1/shorthand.php
@@ -0,0 +1,21 @@
+<?php
+$naam = "Ruben";
+$leeftijd = 20;
+$taal = "Python";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Shorthand PHP</title>
+</head>
+<body>
+
+<h1>Mijn gegevens</h1>
+
+<p>Naam: <?= $naam ?></p>
+<p>Leeftijd: <?= $leeftijd ?></p>
+<p>Favoriete programmeertaal: <?= $taal ?></p>
+
+</body>
+</html>
